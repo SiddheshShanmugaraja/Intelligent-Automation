dca2fc5adfa6959d5ff7a752fd73cf83e134952d
= <=>
2021-06-30 12:51:11 +0530

    doc string for agent.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,16 +1,13 @@
 from pydantic import BaseModel
-from typing import List, Dict, Tuple, Optional
-
+from typing import List
 class Page(BaseModel):
     actions: List[str]
     terminalState: str
     pageName: str
     url: str
-
 class Goal(BaseModel): 
     goal: str
     pages: List[Page]
-
 class Project(BaseModel):
     projectName: str
     username: str   
